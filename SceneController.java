8031ca91e630709d9e5be410d2269e76d6f52f89
deidaraxc4 <tangnick@live.com>
2017-03-31 18:18:59 -0400

    Add files via upload

--- SceneController.java
+++ SceneController.java
@@ -6,15 +6,19 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
 
@@ -30,6 +34,9 @@ public class SceneController implements Initializable{
 	@FXML
 	private TableView table;
 	@FXML
+	private Label label;
+	@FXML
+	private ProgressBar proBar;
 	private TableColumn songCol,artistCol,albumCol;
 	List<File> list;
 	FileChooser fileChooser = new FileChooser();
@@ -40,7 +47,16 @@ public class SceneController implements Initializable{
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
+		volume.setMin(0);
+		volume.setMax(100);
+		volume.setValue(100);
+		volume.valueProperty().addListener(new InvalidationListener() {
+			@Override
+			public void invalidated(Observable observable) {
+				mySong.getMP().setVolume(volume.getValue()/100.0);
+			}
+			
+		});
 		
 	}
 	
@@ -49,16 +65,18 @@ public class SceneController implements Initializable{
 	public void loadFile(ActionEvent event) {
 		Node source = (Node) event.getSource();
 	    Window theStage = source.getScene().getWindow();
+	    //set fileChooser filter
+	    FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("MP3 files", "*.mp3");
+	    fileChooser.getExtensionFilters().add(extFilter);
 		fileChooser.setTitle("Select MP3 files");
-		//File file = fileChooser.showOpenDialog(theStage);
-		//mySong = new Song(file);
-		list = fileChooser.showOpenMultipleDialog(theStage);
+		File file = fileChooser.showOpenDialog(theStage);
+		mySong = new Song(file);
+		/*list = fileChooser.showOpenMultipleDialog(theStage);
 		if(list!=null){
 			for(File x: list) {
 				mySong = new Song(x);
 			}
-		}
-		updateTable();
+		}*/
 	}
 	
 	@FXML
@@ -68,11 +86,22 @@ public class SceneController implements Initializable{
 	
 	@FXML
 	public void stopSong(ActionEvent event) {
-		mySong.pause();
+		//mySong.pause();
+		System.out.println("song title: "+mySong.getArtist());
+		ImageView img = new ImageView(mySong.getCover());
+		//img.fitWidthProperty().bind(label.widthProperty());
+		//img.fitHeightProperty().bind(label.heightProperty());
+		img.setFitHeight(120);
+		img.setFitWidth(200);
+		label.setGraphic(img);
+		//label.setGraphic(new ImageView(mySong.getCover()));
 	}
 	
+	@FXML
+	
+	
 	public void updateTable(){
-		songCol.setText("hi");
+		//songCol.s
 	}
 	
 
--- Song.java
+++ Song.java
@@ -2,23 +2,45 @@ package application;
 
 import java.io.File;
 
+import javafx.collections.MapChangeListener;
+import javafx.collections.MapChangeListener.Change;
+import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class Song {
-	private File file;
 	private String title;
 	private String artist;
 	private String album;
 	private Media music;
 	private MediaPlayer mp;
+	private Image coverArt;
 	
 	public Song(File file) {
 		music = new Media(file.toURI().toString());
+		music.getMetadata().addListener((Change<? extends String, ? extends Object> c) -> {
+	        if (c.wasAdded()) {
+	            if ("artist".equals(c.getKey())) {
+	            	System.out.println(c.getKey()+":"+c.getValueAdded());
+	                artist = c.getValueAdded().toString();
+	            } else if ("title".equals(c.getKey())) {
+	                title = c.getValueAdded().toString();
+	                System.out.println(c.getKey()+":"+c.getValueAdded());
+	            } else if ("album".equals(c.getKey())) {
+	                album = c.getValueAdded().toString();
+	                System.out.println(c.getKey()+":"+c.getValueAdded());
+	            } else if ("image".equals(c.getKey())) {
+	            	coverArt = (Image) c.getValueAdded();
+	            }
+	        }
+	    });
 		mp = new MediaPlayer(music);
-		artist = (String) music.getMetadata().get("artist");
-		title = (String) music.getMetadata().get("title");
-		album = (String) music.getMetadata().get("album");
+		//artist = (String) mp.getMedia().getMetadata().get("artist");
+		//title = (String) music.getMetadata().get("title");
+		//album = (String) music.getMetadata().get("album");
+		//artist = "test";
+		//album = "test";
+		//title = "test";
 	}
 	
 	public void play() {
@@ -43,6 +65,14 @@ public class Song {
 	
 	public String getAlbum(){
 		return album;
+	}
+	
+	public Image getCover(){
+		return coverArt;
+	}
+	
+	public MediaPlayer getMP(){
+		return mp;
 	}
 	
 	
