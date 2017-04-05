698ac8e8f46718bdace5ff644b4f79d3caa0c136
deidaraxc4 <tangnick@live.com>
2017-04-04 22:30:39 -0400

    Update Song.java

--- Song.java
+++ Song.java
@@ -3,6 +3,7 @@ package application;
 import java.io.File;
 
 import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.MapChangeListener;
 import javafx.collections.MapChangeListener.Change;
 import javafx.scene.image.Image;
@@ -10,91 +11,87 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class Song {
-	private String title;
-	private String artist;
-	private String album;
-	private SimpleStringProperty pTitle;
-	private SimpleStringProperty pArtist;
-	private SimpleStringProperty pAlbum;
-	private Media music;
-	private MediaPlayer mp;
-	private Image coverArt;
-	
-	public Song(File file) {
-		music = new Media(file.toURI().toString());
-		music.getMetadata().addListener((Change<? extends String, ? extends Object> c) -> {
-	        if (c.wasAdded()) {
-	            if ("artist".equals(c.getKey())) {
-	            	System.out.println(c.getKey()+":"+c.getValueAdded());
-	            	this.pArtist = new SimpleStringProperty(c.getValueAdded().toString());
-	            	//pArtist.set(c.getValueAdded().toString());
-	                artist = c.getValueAdded().toString();	
-	            } else if ("title".equals(c.getKey())) {
-	                title = c.getValueAdded().toString();
-	                System.out.println(c.getKey()+":"+c.getValueAdded());
-	            } else if ("album".equals(c.getKey())) {
-	                album = c.getValueAdded().toString();
-	                System.out.println(c.getKey()+":"+c.getValueAdded());
-	            } else if ("image".equals(c.getKey())) {
-	            	coverArt = (Image) c.getValueAdded();
-	            }
-	        }
-	    });
-		mp = new MediaPlayer(music);
-		System.out.println(pArtist);
-		System.out.println(artist);
-		//artist = (String) mp.getMedia().getMetadata().get("artist");
-		//title = (String) music.getMetadata().get("title");
-		//album = (String) music.getMetadata().get("album");
-		//artist = "test";
-		//album = "test";
-		//title = "test";
-	}
-	
-	public void play() {
-		mp.play();
-	}
-	
-	public void pause() {
-		mp.pause();
-	}
-	
-	public void stop() {
-		mp.stop();
-	}
-	
-	public String getTitle(){
-		return title;
-	}
-	
-	public void setTitle(String title){
-		this.title = title;
-	}
-	
-	public String getArtist(){
-		return artist;
-	}
-	
-	public void setArtist(String artist){
-		this.artist = artist;
-	}
-	
-	public String getAlbum(){
-		return album;
-	}
-	
-	public void setAlbum(String album){
-		this.album = album;
-	}
-	
-	public Image getCover(){
-		return coverArt;
-	}
-	
-	public MediaPlayer getMP(){
-		return mp;
-	}
-	
+	private final StringProperty title = new SimpleStringProperty();
+    private final StringProperty artist = new SimpleStringProperty();
+    private final StringProperty album = new SimpleStringProperty();
+    private Media music;
+    private MediaPlayer mp;
+    private Image coverArt;
+
+    public Song(File file) {
+        music = new Media(file.toURI().toString());
+        music.getMetadata().addListener((Change<? extends String, ? extends Object> c) -> {
+            if (c.wasAdded()) {
+                if ("artist".equals(c.getKey())) {
+                    setArtist(c.getValueAdded().toString());
+                } else if ("title".equals(c.getKey())) {
+                    setTitle(c.getValueAdded().toString());
+                } else if ("album".equals(c.getKey())) {
+                    setAlbum(c.getValueAdded().toString());
+                } else if ("image".equals(c.getKey())) {
+                    // maybe this needs to be a JavaFX property too: it is not clear from your question:
+                    coverArt = (Image) c.getValueAdded();
+                }
+            }
+        });
+        mp = new MediaPlayer(music);
+    }
+
+    public void play() {
+        mp.play();
+    }
+
+    public void pause() {
+        mp.pause();
+    }
+
+    public void stop() {
+        mp.stop();
+    }
+
+    public StringProperty titleProperty() {
+        return title ;
+    }
+
+    public final String getTitle(){
+        return titleProperty().get();
+    }
+
+    public final void setTitle(String title){
+        titleProperty().set(title);
+    }
+
+    public StringProperty artistProperty() {
+        return artist ;
+    }
+
+    public final String getArtist(){
+        return artistProperty().get();
+    }
+
+    public final void setArtist(String artist){
+        artistProperty().set(artist);
+    }
+
+    public StringProperty albumProperty() {
+        return album ;
+    }
+
+    public final String getAlbum(){
+        return albumProperty().get();
+    }
+
+    public final void setAlbum(String album){
+        albumProperty().set(album);
+    }
+
+    public Image getCover(){
+        return coverArt;
+    }
+
+    public MediaPlayer getMP(){
+        return mp;
+    }
 	
 	
 	
